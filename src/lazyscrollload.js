d85be2a25c0aa21fe101ccd3b8e073b0092d5eae
Bellamkonda Bharat Kumar <pavinan@users.noreply.github.com>
2017-01-17 17:52:02 +0530

    added loadash and updated

--- src/lazyscrollload.js
+++ src/lazyscrollload.js
@@ -1,1 +1,15 @@
-//aa
+
+(function ($) {
+    $.fn.lazyScrollLoad = function () {
+        var elements = this;
+        return elements.each(function () {
+            var $el = $(this);
+            $el.on('scroll', _.throttle(function () {
+                var $this = $(this);
+                if (this.scrollHeight - this.scrollTop === this.clientHeight) {
+                    console.log(true)
+                }
+            }, 300));
+        })
+    };
+})(jQuery)
