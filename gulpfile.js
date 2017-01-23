702f51de6304e94bc1cfcd6a1f82e323d67fcdfe
Bellamkonda Bharat Kumar <pavinan@users.noreply.github.com>
2017-01-23 18:43:12 +0530

    added gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
 
-var gulp = require("gulp"), connect = require("gulp-connect");
+var gulp = require("gulp"),
+    clean = require("gulp-clean"),
+    uglify = require("gulp-uglify"),
+    connect = require("gulp-connect");
 
-gulp.task("serve", function () {
+gulp.task("serve", function() {
     return connect.server({
         port: 4040
     });
 });
 
+gulp.task("clean", function() {
+    return gulp.src("dist", { read: false })
+        .pipe(clean())
+});
+
+gulp.task("minify", function() {
+    return gulp.src("src/lazyscrollload.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("dist"));
+})
+
+gulp.task("copy", function() {
+    return gulp.src("src/lodash.custom.min.js")
+        .pipe(gulp.dest("dist"))
+})
+
+gulp.task("build", ["clean", "minify", "copy"], function() {
+
+});
